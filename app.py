cf8f010ece813d297d9decd0aac68b303ba3227c
Harshitaloner <livehr2008@gmail.com>
2026-09-13 09:09:58 +0530

    Update app.py

--- app.py
+++ app.py
@@ -235,3 +235,4 @@ def generate_default_analysis_mockup() -> dict:
             "ERROR: IKE Notify NO_PROPOSAL_CHOSEN - Encryption algorithm mismatch",
             "WARNING: Weak DH Group detected (GROUP2-1024) - Upgrade to GROUP14+",
             ]
+    }
